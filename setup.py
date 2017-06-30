fdb7d21d65d8e56f9ab33c66afb67fa0d2da3033
Findeton <felix@nvotes.com>
2017-06-30 10:54:37 +0200

    release go-v1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from pip.req import parse_requirements
 
 setup(
     name='Agora Tally',
-    version='17.04',
+    version='103.111.1',
     author='Eduardo Robles Elvira',
     author_email='edulix@nvotes.com',
     packages=['agora_tally', 'agora_tally.voting_systems'],
