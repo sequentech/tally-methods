567d378bd9ab20ca88d1a73b849310c9ca10e856
Eduardo Robles Elvira <edulix@gmail.com>
2021-09-12 21:26:04 +0200

    Release for version 4.0.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import setup
 
 setup(
     name='agora-tally',
-    version='4.0.0-beta.1',
+    version='4.0.0',
     author='Agora Voting SL',
     author_email='contact@nvotes.com',
     packages=['agora_tally', 'agora_tally.voting_systems'],
