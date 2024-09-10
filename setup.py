dd168e73f8df7670fe532e6b2c34877755fb86cf
Findeton <felix@sequentech.io>
2024-09-10 18:34:06 -0500

    Release for version 10.5.1

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import setup
 
 setup(
     name='tally-methods',
-    version='10.4.0',
+    version='10.5.1',
     author='Sequent Tech Inc',
     author_email='legal@sequentech.io',
     packages=['tally_methods', 'tally_methods.voting_systems'],
